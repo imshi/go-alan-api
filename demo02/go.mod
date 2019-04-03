8e58456f114b1a1c8cdfee7e42c0b9d504b5990b
shi.wang <im_wangshi@163.com>
2019-04-03 16:23:14 +0800

    Push existing floder to init go-alan-api

---
+++ demo02/go.mod
@@ -0,0 +1,21 @@
+module demo02
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/StackExchange/wmi v0.0.0-20181212234831-e0a55b97c705 // indirect
+	github.com/fsnotify/fsnotify v1.4.7
+	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
+	github.com/gin-gonic/gin v1.3.0
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/json-iterator/go v1.1.6 // indirect
+	github.com/mattn/go-isatty v0.0.7 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/shirou/gopsutil v2.18.12+incompatible
+	github.com/spf13/pflag v1.0.3
+	github.com/spf13/viper v1.3.2
+	golang.org/x/net v0.0.0-20190328230028-74de082e2cca // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+)
---
+++ sample-api/main.go
@@ -0,0 +1,64 @@
+package main
+
+///////////////////////
+// 实现一个基础的RESTfulAPI服务器 //
+///////////////////////
+import (
+	"errors"
+	"log"
+	"net/http"
+	"sample-api/router"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+///////////////////////////////////////////////////
+// main()为入口函数，主要做一些配置文件解析、程序初始化和路由加载之类的事 //
+// 最终调用 http.ListenAndServe() 在指定端口启动一个 HTTP 服务器 //
+///////////////////////////////////////////////////
+func main() {
+	// 创建一个不包含中间件的Gin实例
+	g := gin.New()
+
+	// 定义容纳多个中间件（gin.HandlerFunc{}类型）的Slice
+	middlewares := []gin.HandlerFunc{}
+
+	// main() 函数通过调用 router.Load 函数来加载路由，函数路径为 router/router.go
+	router.Load(
+		// Cores.
+		g,
+
+		// 中间件，middlewares...表示为可变参数middlewares传参
+		middlewares...,
+	)
+
+	// Ping the server to make sure the router is working.
+	go func() {
+		if err := pingServer(); err != nil {
+			log.Fatal("The router has no response, or it might took too long to start up.", err)
+		}
+		log.Print("The router has been deployed successfully.")
+	}()
+
+	log.Printf("Start to listening the incoming requests on http address: %s", ":8080")
+	// 调用 http.ListenAndServe() 在指定端口启动一个 HTTP 服务器，并输出错误信息
+	log.Printf(http.ListenAndServe(":8080", g).Error())
+}
+
+// pingServer pings the http server to make sure the router is working.
+// pingServer函数向/sd/health发送GET请求，如果函数正确执行并且返回的 HTTP StatusCode 为 200，则说明 API 服务器可用，pingServer函数输出部署成功提示；如果超过指定次数，pingServer 直接终止 API Server 进程
+func pingServer() error {
+	for i := 0; i < 2; i++ {
+		// Ping the server by sending a GET request to `/health`.
+		resp, err := http.Get("http://127.0.0.1:8080" + "/sd/health")
+		if err == nil && resp.StatusCode == 200 {
+			return nil
+		}
+
+		// Sleep for a second to continue the next ping.
+		log.Print("Waiting for the router, retry in 1 second.")
+		time.Sleep(time.Second)
+	}
+	return errors.New("Cannot connect to the router.")
+}
