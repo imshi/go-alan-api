58c66f1377af4b9685bf55d5dc97b7203c086ebd
shi.wang <im_wangshi@163.com>
2019-04-05 19:39:37 +0800

    add demo for import pravite packages from aliCode

---
+++ private-package/go.mod
@@ -0,0 +1,3 @@
+module private-package
+
+require code.aliyun.com/ecarx_ops/go-private v0.0.0-20190403114509-feb199d613c6 // indirect
---
+++ private-package/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+
+    "code.aliyun.com/ecarx_ops/go-private/common"
+)
+
+func main() {
+    common.Hello()
+    fmt.Printf("this is fmt")
+}
