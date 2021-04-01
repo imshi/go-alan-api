ecf30b2ce02581d2c29baae07eaf2f38840ad218
shi.wang <im_alan@163.com>
2021-04-01 13:38:04 +0800

    replace log module

--- diy-errno/handler/user/create.go
+++ diy-errno/handler/user/create.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/lexkong/log"
+	"github.com/sirupsen/logrus"
 )
 
 // 用户模块业务处理函数 1：创建用户
@@ -24,14 +24,14 @@ func Create(c *gin.Context) {
 		return
 	}
 
-	log.Debugf("username is :[%s],password is [%s]", r.Username, r.Password)
+	logrus.Debugf("username is :[%s],password is [%s]", r.Username, r.Password)
 	if r.Username == "" {
 		err = errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in dh: x.x.x.x")).Add("This is add message.")
-		log.Errorf(err, "Get an error")
+		logrus.Errorf("Get an error：%s", err)
 	}
 
 	if errno.IsErrUserNotFound(err) {
-		log.Debug("err type is ErrUserNotFound")
+		logrus.Debug("err type is ErrUserNotFound")
 	}
 
 	if r.Password == "" {
