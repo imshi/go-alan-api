b9c6d9964a82f5af4a2a5dff9d39bcb046b55b03
shi.wang <im_alan@163.com>
2021-04-07 09:52:31 +0800

    update do-curd

--- do-curd/handler/user/list.go
+++ do-curd/handler/user/list.go
@@ -10,18 +10,19 @@ import (
 
 // 从数据库中获取用户列表
 func List(c *gin.Context) {
-	var r ListResult
+	var r ListRequest
 	if err := c.Bind(&r); err != nil {
 		SendResponse(c, errno.ErrBind, nil)
 		return
 	}
-	infos, count, err := service.ListUsers(r.Username, r.Offset, r.Limit)
+	// 分页查询，收到的请求中传入的 offset 和 limit 参数，分别对应于 MySQL 的 offset 和 limit
+	infos, count, err := service.ListUser(r.Username, r.Offset, r.Limit)
 	if err != nil {
 		SendResponse(c, err, nil)
 		return
 	}
 	SendResponse(c, nil, ListResponse{
-		TocalCount: count,
+		TotalCount: count,
 		UserList:   infos,
 	})
 }
